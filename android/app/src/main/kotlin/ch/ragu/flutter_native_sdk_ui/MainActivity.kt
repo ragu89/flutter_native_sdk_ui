ea71041b2cbc91c9a1073cb012f64c8b5b155dfe
Guye Raphael, I233 <raphael.guye@post.ch>
2020-12-07 09:21:31 +0100

    android implementation for openCamera

--- android/app/src/main/kotlin/ch/ragu/flutter_native_sdk_ui/MainActivity.kt
+++ android/app/src/main/kotlin/ch/ragu/flutter_native_sdk_ui/MainActivity.kt
@@ -1,6 +1,64 @@
 package ch.ragu.flutter_native_sdk_ui
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import ly.img.android.pesdk.PhotoEditorSettingsList
+import ly.img.android.pesdk.assets.filter.basic.FilterPackBasic
+import ly.img.android.pesdk.assets.font.basic.FontPackBasic
+import ly.img.android.pesdk.assets.frame.basic.FramePackBasic
+import ly.img.android.pesdk.assets.overlay.basic.OverlayPackBasic
+import ly.img.android.pesdk.assets.sticker.emoticons.StickerPackEmoticons
+import ly.img.android.pesdk.assets.sticker.shapes.StickerPackShapes
+import ly.img.android.pesdk.ui.activity.CameraPreviewBuilder
+import ly.img.android.pesdk.ui.model.state.*
+
 
 class MainActivity: FlutterActivity() {
+
+    companion object {
+        const val PESDK_RESULT = 1
+    }
+
+    private val CHANNEL = "ch.ragu.flutter_native_sdk/photoEditorChannel"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "openCamera") {
+                this.openCamera()
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun openCamera() {
+        val settingsList = createPESDKSettingsList()
+
+        CameraPreviewBuilder(this)
+                .setSettingsList(settingsList)
+                .startActivityForResult(this, PESDK_RESULT)
+    }
+
+    private fun createPESDKSettingsList() = PhotoEditorSettingsList()
+            .configure<UiConfigFilter> {
+                it.setFilterList(FilterPackBasic.getFilterPack())
+            }
+            .configure<UiConfigText> {
+                it.setFontList(FontPackBasic.getFontPack())
+            }
+            .configure<UiConfigFrame> {
+                it.setFrameList(FramePackBasic.getFramePack())
+            }
+            .configure<UiConfigOverlay> {
+                it.setOverlayList(OverlayPackBasic.getOverlayPack())
+            }
+            .configure<UiConfigSticker> {
+                it.setStickerLists(
+                        StickerPackEmoticons.getStickerCategory(),
+                        StickerPackShapes.getStickerCategory()
+                )
+            }
 }
